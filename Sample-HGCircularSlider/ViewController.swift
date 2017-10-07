7d17ef9672d741b25be49712f62b78de020f821a
takumi314 <isospin_symmetry@me.com>
2017-10-08 02:10:17 +0900

    first commit

---
+++ Sample-HGCircularSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample-HGCircularSlider
+//
+//  Created by NishiokaKohei on 2017/10/08.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
