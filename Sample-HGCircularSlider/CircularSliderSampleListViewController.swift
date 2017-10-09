6350dba1a3686482f98d4de52a083730250fa345
takumi314 <isospin_symmetry@me.com>
2017-10-09 16:02:14 +0900

    add CircularSliderSampleListViewController

---
+++ Sample-HGCircularSlider/CircularSliderSampleListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSliderSampleListViewController.swift
+//  Sample-HGCircularSlider
+//
+//  Created by NishiokaKohei on 2017/10/09.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import UIKit
+
+class CircularSliderSampleListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- Sample-HGCircularSlider/MainViewController.swift
+++ Sample-HGCircularSlider/MainViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: -  Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+
+// MARK: - NSObject
+
+extension NSObject: Identifiable { }
+
+// MARK: - Protocols
+
+protocol Identifiable {
+    func identifier() -> String
+}
+
+extension Identifiable {
+    func identifier() -> String {
+        return "\(self)"
+    }
+}
