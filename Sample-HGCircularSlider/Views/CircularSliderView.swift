7a582a29c007a35a9574cbba85f2f2bd15a3e31f
takumi314 <isospin_symmetry@me.com>
2017-10-10 01:24:42 +0900

    add new View class

---
+++ Sample-HGCircularSlider/Views/CircularSliderView.swift
@@ -0,0 +1,28 @@
+//
+//  CircularSliderView.swift
+//  Sample-HGCircularSlider
+//
+//  Created by NishiokaKohei on 2017/10/10.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import UIKit
+import HGCircularSlider
+
+class CircularSliderView: CircularSlider {
+
+    // MARK: - Initilizer
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: -
+
+
+}
