cca4730e6bf05ee36dee58af3621fd1f150564cb
takumi314 <isospin_symmetry@me.com>
2017-10-09 21:30:04 +0900

    make new classes

---
+++ Sample-HGCircularSlider/Presenters/CircleSliderPresenter.swift
@@ -0,0 +1,19 @@
+//
+//  CircularSliderPresenter.swift
+//  Sample-HGCircularSlider
+//
+//  Created by NishiokaKohei on 2017/10/09.
+//  Copyright © 2017年 Kohey.N. All rights reserved.
+//
+
+import UIKit
+import HGCircularSlider
+
+class CircularSliderPresenter: UIView {
+
+    func instatiate(_ frame: CGRect) -> UIView {
+        return CircularSlider(frame: frame)
+    }
+
+
+}
