dcff83d6760d9a486c772bc903fbeb446e0c2841
takumi314 <isospin_symmetry@me.com>
2017-10-10 01:06:23 +0900

    To do Presenter class

--- Sample-HGCircularSlider/Presenters/CircleSliderPresenter.swift
+++ Sample-HGCircularSlider/Presenters/CircleSliderPresenter.swift
@@ -9,10 +9,82 @@
 import UIKit
 import HGCircularSlider
 
-class CircularSliderPresenter: UIView {
+class CircularSliderPresenter: NSObject {
 
-    func instatiate(_ frame: CGRect) -> UIView {
-        return CircularSlider(frame: frame)
+    var circularSlider: CircularSlider
+
+    private var frame: CGRect {
+        didSet {
+            self.circularSlider.frame = frame
+        }
+    }
+
+    // MARK: - Initilizer
+
+    private override init() {
+        self.circularSlider = CircularSlider()
+        self.frame = CGRect(x: 0.0, y: 0.0, width: 0.0, height: 0.0)
+    }
+
+    ///
+    /// Required to cell this initializer
+    ///
+    convenience init(_ viewController: UIViewController) {
+        self.init()
+        self.frame = viewController.view.frame
+    }
+
+    // MARK: - Public methods
+
+    func instatiate() -> UIView {
+        return circularSlider
+    }
+
+    func update(for viewController: UIViewController) {
+        viewController.view.setNeedsDisplay()
+        if let vc = viewController as? SimplestCircularSliderViewController {
+            vc.wholeView?.setNeedsDisplay()
+        }
+    }
+
+    func config(_ minimum: CGFloat, _ maximum: CGFloat, _ end: CGFloat)  {
+        circularSlider.minimumValue = minimum
+        circularSlider.maximumValue = maximum
+        circularSlider.endPointValue = end
+    }
+
+    ///
+    /// set up all the color about HGCircularSlider
+    ///
+    func paint() -> Void {
+        paintForDisk()
+        paintForTrack()
+    }
+
+    ///
+    /// To interact
+    ///
+    func interect() {
+    }
+
+    // MARK: - Private methods
+
+    private func paintForDisk(_ diskColor: UIColor = .gray, _ diskFillColor: UIColor = .clear) {
+        circularSlider.diskColor = diskColor
+        circularSlider.diskFillColor = diskFillColor
+    }
+
+    private func paintForTrack(_ trackColor: UIColor = .white, trackFillColor: UIColor = .clear) {
+        circularSlider.trackColor = trackColor
+        circularSlider.trackFillColor = trackFillColor
+    }
+
+    private func change(width: CGFloat = 5.0) {
+        circularSlider.lineWidth = width
+    }
+
+    private func changeCircle() {
+
     }
 
 
